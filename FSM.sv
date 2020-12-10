4e0a30ca5f31338b2da4ca1813a2c175f2c8d057
jaehank2 <74538293+jaehank2@users.noreply.github.com>
2020-12-10 17:55:08 +0900

    game fully functioning and commented

--- FSM.sv
+++ FSM.sv
@@ -1,14 +1,14 @@
 module FSM(input logic Reset, Clk,
            input [7:0] keycode,
-           input over, win,
-	   output reseton,
+           input over, win,	// over = game lost, win = game win
+	   output reseton,	// reset after game ends
            output [1:0] level
           );
         
 
 	enum logic [2:0] {RES, START, OVER, WIN} curr_state, next_state;
         logic [7:0] Enter;
-        assign Enter = 8'h28;
+        assign Enter = 8'h28;	// keycode for ENTER
 	logic next_reseton;
 
         always_ff @ (posedge Clk)
@@ -27,8 +27,9 @@ module FSM(input logic Reset, Clk,
 
         always_comb
         begin
-        next_state = curr_state;
+            next_state = curr_state;
 	    next_reseton = reseton;
+		
             case (curr_state)
 	    RES : begin
 		next_reseton = 1'b0;
@@ -36,44 +37,46 @@ module FSM(input logic Reset, Clk,
 		next_state = START;
 	    end
 		 
-		 //START
+	    //START
             START : begin
-					next_reseton = 1'b0;
+		next_reseton = 1'b0;
                 level = 2'b01;
                 if (over)
-						begin
+		begin
                     next_state = OVER;
-						end
-					 else if(win)
-					 begin 
-						next_state= WIN;
-					 end
+		end
+		else if(win)
+		begin 
+		    next_state= WIN;
+		end
                 else
-						begin 
+		begin 
                     next_state = START;
-						end 
-					end
+		end 
+	    end
 		
-	//WIN
-			WIN: begin
+	    //WIN
+	    WIN: begin
 		 level = 2'b11;
 		 if(keycode==Enter)
 		 begin
 			next_state= RES;
-			next_reseton = 1'b1;
-			end
-			else
-			begin
+			next_reseton = 1'b1;	// when game ends and ENTER pressed, reset
+		end
+		else
+		begin
 			next_state = WIN;
-			end
-			end
-			//OVER
+			next_reseton = 1'b0;
+		end
+	    end
+	
+	    //OVER
             OVER : begin
                 level = 2'b10;
                 if (keycode == Enter)
 		begin
                     next_state = RES;
-		    next_reseton = 1'b1;
+		    next_reseton = 1'b1;	// when game ends and ENTER pressed, reset
                 end
 		else 
 		begin
