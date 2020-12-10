a9ad668ef9753ecda5252dcbd831644d08429510
jaehank2 <74538293+jaehank2@users.noreply.github.com>
2020-12-11 00:53:28 +0900

    Game fully functioning and commented

--- wingame.sv
+++ wingame.sv
@@ -3,16 +3,16 @@ module wingame(input logic [95:0] array,
 	
 	
 	// array[i] = 1 for i in [0, 95] if dot_i consumed				
-		always_comb
+	always_comb
+	begin
+		if(array ==  96'b111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111)
 		begin
-			if(array ==  96'b111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111)
-			begin
-				win = 1'b1;
-			end
-			else
-			begin
-				win = 1'b0;
-			end
+			win = 1'b1;
 		end
+		else
+		begin
+			win = 1'b0;
+		end
+	end
 				
 endmodule
